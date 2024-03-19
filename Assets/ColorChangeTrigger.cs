1e57e938e93e6c219a925bd6c47c57a23caa8127
Usman <usman_6320@hotmail.com>
2024-03-19 21:57:39 +0500

    MazeRoomImplemented

---
+++ Assets/ColorChangeTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeTrigger : MonoBehaviour
+{
+    public static event System.Action OnObjectTriggered; 
+
+    private bool isTriggered = false; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isTriggered) 
+        {
+            Debug.Log("Triggered");
+            GetComponent<Renderer>().material.color = Color.green; // Change color to green
+            isTriggered = true;
+            OnObjectTriggered?.Invoke(); // Notify that this object has been triggered
+        }
+    }
+}
